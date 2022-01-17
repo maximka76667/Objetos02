94f4b7866610d29ee1ed16bac85bab54369c7a3e
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-01-17 10:56:41 +0100

    ej05 artista

--- src/ej05_artista/Artista.java
+++ src/ej05_artista/Artista.java
@@ -1,5 +1,50 @@
 package ej05_artista;
 
+import java.time.LocalDate;
+
 public class Artista {
 
+	private String nombre, pais;
+	private int anoNacimiento;
+	
+	public Artista(String nombre, String pais, int anoNacimiento) {
+		this.nombre = nombre;
+		this.pais = pais;
+		this.anoNacimiento = anoNacimiento;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public String getPais() {
+		return this.pais;
+	}
+	
+	public int getAnoNacimiento() {
+		return this.anoNacimiento;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	
+	public void setNombre(int anoNacimiento) {
+		this.anoNacimiento = anoNacimiento;
+	}
+	
+	public int getEdad() {
+		return LocalDate.now().getYear() - this.anoNacimiento;
+	}
+	
+	public void printInfo() {
+		System.out.println("Nombre: " + this.nombre);
+		System.out.println("Pais: " + this.pais);
+		System.out.println("Ano de nacimiento: " + this.anoNacimiento);
+		System.out.println("Edad: " + getEdad());
+	}
 }
---
+++ src/ej05_artista/Main.java
@@ -0,0 +1,11 @@
+package ej05_artista;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Artista artista = new Artista("", "", 2000);
+		
+		artista.printInfo();
+	}
+
+}
